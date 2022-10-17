00f94aa9416739d2d45fccfaeb5b8c745993ce97
Lê Thị Thuỳ Dung <lethithuydung_t65@hus.edu.vn>
2022-10-17 14:13:11 +0700

    set api url

--- src/Utils/Config/getApiUrl.js
+++ src/Utils/Config/getApiUrl.js
@@ -3,7 +3,7 @@
  */
 export const getApiUrl = (environment = "production") => {
   // if (environment === "production") {
-  //   return "https://jvb-kanji-server.herokuapp.com/"
+  //   return "https://jvb-kanji-server.herokuapp.com"
   // }     
-  return "https://jvb-kanji-server.herokuapp.com/"
+  return "https://jvb-kanji-server.herokuapp.com";
 };
