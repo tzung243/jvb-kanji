97c46082851bc2a6f12306f7ed768d2953ca62c7
Lê Thị Thuỳ Dung <lethithuydung_t65@hus.edu.vn>
2022-10-27 12:19:31 +0700

    text blog

--- src/Components/Widgets/BlogInHome.jsx
+++ src/Components/Widgets/BlogInHome.jsx
@@ -6,6 +6,7 @@ export const BlogInHome = () => {
     <section className="w-full flex justify-center">
       <main className="lg:w-lg w-full flex flex-col space-y-12 justify-center items-center py-4">
         <p className="text-gray-700 font-bold text-3xl">Blog</p>
+        <p className="text-base font-light text-gray-500 text-center m-4">With Chip Chip blog, you and other users can share your knowledge and experience of Japan with a large community. Users will be able to learn interesting facts, save the tricks to take the JLPT exam with the highest score and connect with like-minded friends.</p>
         <div className="flex flex-col lg:flex-row lg:space-y-0 space-y-4 space-x-2 items-center justify-center transition-all duration-300">
           <img src={UndrawLearningSketching} alt="" className="w-96" />
           <a
